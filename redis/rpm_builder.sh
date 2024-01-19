f7569fd910012bf89cf0fae83895bde045b5e845
Alan Peng <peng.alan@gmail.com>
2024-01-19 10:51:17 -0500

    update scripts

--- redis/rpm_builder.sh
+++ redis/rpm_builder.sh
@@ -3,8 +3,8 @@
 set -e
 
 name='redis'
-version='6.0.15'
-core_branch='release-2.3.0'
+version='7.0.9'
+core_branch='release-2.10.0'
 
 function checkdep {
 	if ! wget --version &> /dev/null
@@ -24,11 +24,11 @@ mkdir -p $workDir && cd $workDir
 wget http://download.redis.io/releases/$name-$version.tar.gz
 
 # step 2: get redis-conf.patch from upstream
-wget https://raw.githubusercontent.com/vmware/photon/4.0/SPECS/redis/redis-conf.patch
+wget https://raw.githubusercontent.com/vmware/photon/5.0/SPECS/redis/redis-conf.patch
 
-# step 3: get spec builder script, and replace version to 4, then to build the redis rpm packages
-wget https://raw.githubusercontent.com/vmware/photon/4.0/tools/scripts/build_spec.sh
-sed "s|VERSION=3|VERSION=4|g" -i build_spec.sh
+# step 3: get spec builder script, and replace version to 5, then to build the redis rpm packages
+wget https://raw.githubusercontent.com/vmware/photon/5.0/tools/scripts/build_spec.sh
+sed "s|VERSION=4|VERSION=5|g" -i build_spec.sh
 chmod 755 ./build_spec.sh && cp $cur/redis.spec .
 
 # step 4: build redis rpm.
